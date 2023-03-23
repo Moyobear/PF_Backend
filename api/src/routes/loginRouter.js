2672dc1e9c5866d223b6733ae5de5c5e692d233c
Santiago Mendoza <srjmendoza@hotmail.com>
2023-03-23 13:05:45 -0300

    creando el codigo para login con Auth0

---
+++ api/src/routes/loginRouter.js
@@ -0,0 +1,18 @@
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:3001',
+  clientID: 'M1EiTbA5Hz0TM6oZWqY4j0FuTCjqZMUk',
+  issuerBaseURL: 'https://dev-5f62ukzogfdcpj7n.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
