20e83206986a0c2f66a79e4ef27ffcfbbd90d477
Jonathan Berna <jonathan_3_7@hotmail.com>
2023-03-24 11:38:28 -0500

    Se modifico el archivo DB

--- api/src/db.js
+++ api/src/db.js
@@ -15,7 +15,7 @@ require("dotenv").config();
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://postgres:bearfreelance@localhost:5432/clinica`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
